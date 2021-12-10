4c59901211b0ccb8a6a1b1651b2d15f2520986b4
Naveen Kanakatte <naveenkn@stanford.edu>
2021-12-09 22:40:34 -0800

    new page

--- JS/resume.js
+++ JS/resume.js
@@ -1,7 +1,7 @@
-const url = "https://contactform-project.000webhostapp.com/my_positions.json";
+const positionUrl = "https://contactform-project.000webhostapp.com/my_positions.json";
 
 function fetchPositions() {
-  fetch(url)
+  fetch(positionUrl)
     .then(response => {
       if (!response.ok) {
         throw new Error('An issue with fetching the data.');
@@ -10,10 +10,10 @@ function fetchPositions() {
     })
     .then(function (data) {
       const display = displayPositions(data.my_job_positions.positions);
-      document.getElementById('responsedata').innerHTML = display;
+      document.getElementById('positiondata').innerHTML = display;
     })
     .catch(error => {
-      document.getElementById('responsedata').innerHTML = "An issue with fetching the data. Please try again after sometime.";
+      document.getElementById('positiondata').innerHTML = "An issue with fetching the data. Please try again after sometime.";
     });
 }
 
