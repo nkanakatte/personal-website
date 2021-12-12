b2416897a37a7286e777127290558cd31f7ad82b
Naveen Kanakatte <naveenkn@stanford.edu>
2021-12-11 16:33:17 -0800

    print styles

--- JS/main.js
+++ JS/main.js
@@ -2,8 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         myName: 'Naveen Kanakatte',
+        website:'http://naveen-kanakatte.me',
         profileImage: './Assets/Naveen.jpg',
         maxLength: 200,
+        displayPMIBadge: true,
+        hover: false,
+        displayAWSBadge: true,
         value: '',
         degrees: [
             { school: "BU Metropolitan College", program: "Software Development", type: "Master's", year: "May 2022" },
@@ -11,7 +15,9 @@ var app = new Vue({
             { school: "Stanford Graduate School of Business", program: "\"Executive Program for Leadership: An effective use of Power\"", type: "Executive Education Course", year: "September 2017" },
         ]
     },
-    printWindow: function () {
-        window.print();
-    }
-})
+    methods: {
+        printWindow: function () {
+            window.print();
+        }
+    },
+});
